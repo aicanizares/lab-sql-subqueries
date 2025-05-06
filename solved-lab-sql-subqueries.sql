ebe5ce5daa8c3216722961cea2c3a9bc3be768bb
Ana Cañizares <canizares.alvarez.ana@gmail.com>
2025-05-06 14:18:42 +0200

    solved-lab-sql-subqueries-3

--- solved-lab-sql-subqueries.sql
+++ solved-lab-sql-subqueries.sql
@@ -5,6 +5,13 @@ INNER JOIN film
 ON inventory.film_id = film.film_id
 WHERE title = "Hunchback Impossible";
 
+# The avobe code is using joins. This exercise asks to use subqueries. See code below.
+SELECT COUNT(inventory_id) AS number_of_copies
+FROM inventory
+WHERE film_id = ( SELECT film_id
+						FROM film
+						WHERE title = "Hunchback Impossible" ) ;
+
 # 2. List all films whose length is longer than the average length of all the films in the Sakila database.
 # END RESULT
 SELECT title
@@ -33,3 +40,31 @@ USING (actor_id)
 WHERE film_id = ( SELECT film_id
 					FROM film
 					WHERE title = "Alone Trip" );
+                    
+# 6. Determine which films were starred by the most prolific actor in the Sakila database. A prolific actor is defined as the actor who has acted in the most number of films. First, you will need to find the most prolific actor and then use that actor_id to find the different films that he or she starred in.
+SELECT title
+FROM film
+WHERE film_id IN ( 	SELECT film_id
+					FROM film_actor
+					WHERE actor_id = ( SELECT actor_id
+										from film_actor
+										GROUP BY actor_id
+										ORDER BY COUNT(film_id) DESC
+										LIMIT 1 )) ;
+                                        
+# 8. Retrieve the client_id and the total_amount_spent of those clients who spent more than the average of the total_amount spent by each client. You can use subqueries to accomplish this.
+# above_average_spenders
+
+SELECT customer_id, SUM(amount) AS tspc
+FROM payment
+GROUP BY customer_id
+HAVING tspc > ( SELECT AVG(ts)
+					FROM 	(SELECT SUM(amount) ts
+					FROM payment
+					GROUP BY customer_id) AS spent );
+###############
+
+SELECT AVG(ts)
+FROM 	(SELECT SUM(amount) ts
+		FROM payment
+		GROUP BY customer_id) AS spent;
